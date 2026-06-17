2385f2a4a2e4147b21f324d799e1f9fae16f279c
Ananya Jan <ananyajan2000@gmail.com>
2026-06-17 14:10:13 +0530

    Leadform,Leadpage

---
+++ backend/controllers/leadController.js
@@ -0,0 +1,29 @@
+﻿import Lead from "../models/Lead.js";
+
+export const getLeads = async (req, res) => {
+  try {
+    const leads = await Lead.find().sort({ createdAt: -1 });
+    res.status(200).json({ success: true, data: leads });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const createLead = async (req, res) => {
+  try {
+    const { name, email, phone, address, disposition = "Quoted", notes = "" } = req.body;
+
+    const lead = await Lead.create({
+      name,
+      email,
+      phone,
+      address,
+      disposition,
+      notes,
+    });
+
+    res.status(201).json({ success: true, data: lead });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-import express from "express";
+﻿import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import connectDB from "./config\/db.js";
+import dns from 'dns';
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+
+import connectDB from "./config/db.js";
+import leadRoutes from "./routes/leadRoutes.js";
 
 dotenv.config();
+console.log(process.env.MONGO_URI);
 
 const app = express();
 
@@ -15,12 +20,13 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/leads", leadRoutes);
 
 // Test Route
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: "Backend Running 🚀",
+    message: "Backend Running ðŸš€",
   });
 });
 
@@ -31,3 +37,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
